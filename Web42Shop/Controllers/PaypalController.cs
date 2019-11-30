bd52289daee74154b338d363c9dbd83669c1412f
nsuong <nguyensuong1918@gmail.com>
2019-11-30 15:32:26 +0700

    Suong mới làm paypal ai rảnh chỉnh css hộ S nhé, cứ demo trước nha

---
+++ Web42Shop/Controllers/PaypalController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+namespace Web42Shop.Controllers
+{
+    public class PaypalController : Controller
+    {
+        IConfiguration _iconfiguration;
+        public PaypalController(IConfiguration iconfiguration)
+        {
+            _iconfiguration = iconfiguration;
+        }
+        public ActionResult PaypalView(string item, string sum)
+        {
+            //sương
+            Web42Shop.Models.PaypalModel paypal = new Models.PaypalModel();
+            paypal.cmd = "_xclick";
+            paypal.business = _iconfiguration.GetSection("appSettings").GetSection("BusinessAccountKey").Value;
+
+            bool useSandbox = Convert.ToBoolean(_iconfiguration.GetSection("appSettings").GetSection("UseSandbox").Value);
+            if (useSandbox)
+            {
+                ViewBag.actionURL = "https://www.sandbox.paypal.com/cgi-bin/webscr";
+            }
+            else
+            {
+                ViewBag.actionURL = "https://www.paypal.com/cgi-bin/webscr";
+            }
+
+            paypal.cancel_return = System.Configuration.ConfigurationManager.AppSettings["CancelURL"];
+            paypal.@return = ConfigurationManager.AppSettings["ReturnURL"];
+            paypal.notify_url = ConfigurationManager.AppSettings["NotifyURL"];
+            paypal.currency_code = ConfigurationManager.AppSettings["CurrencyCode"];
+
+            paypal.item_name = item;
+            paypal.amount = sum;
+            return View(paypal);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
